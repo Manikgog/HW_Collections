3fd7669ecbbefc9fb0fc44dff5d24fe5c7399274
Manikgog <m4xin4t4@gmail.com>
2023-11-21 13:36:13 +0500

    Initial commit

---
+++ src/main/java/com/collections/homeworkCollections/HomeworkCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.collections.homeworkCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCollectionsApplication.class, args);
+	}
+
+}
